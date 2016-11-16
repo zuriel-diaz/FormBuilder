eeb06a338d7a8a2cb20768cf178429ad701b06cd
Zuriel.Diaz <zuriel.diaz@gea.mx>
2016-11-16 11:17:24 -0600

    first iteration: generate input fields.

--- resources/js/form-builder.js
+++ resources/js/form-builder.js
@@ -6,12 +6,72 @@ class FormBuilder{
 		this.method 	= data.method;
 	}
 
-	setFields(fields){
-		console.log(fields);
+	generateField(data){
+		var node 		= null;
+		var label 		= null;
+		var input		= null;
+		var text 		= null;
+
+		switch(data.type){
+
+			case 'input':
+
+				// generate label
+				label 	= document.createElement('label');
+				label.setAttribute('for',data.name);
+				text 	= document.createTextNode(data.name);
+				label.appendChild(text);
+				
+				// generate input 
+				input	= document.createElement('input');
+				input.setAttribute('type',data.others.input_type);
+				input.setAttribute('id',data.name);
+				input.setAttribute('class','form-control');
+				input.setAttribute('placeholder',data.others.placeholder);
+
+				// generate div container & set elements
+				node 	= document.createElement('div');
+				node.setAttribute('class','form-group');
+				node.appendChild(label);
+				node.appendChild(input);
+			break;
+		}
+
+		return node;
 	}
 
+	// save fields data
+	setFields(_fields){this.fields=_fields;}
+
+	// Basically here is the key operation.
 	generate(container_identifier){
-		console.log('container identifier->'+container_identifier);
+		
+		// get container
+		var container 	= document.getElementById(container_identifier);
+
+		// create form element & set attributes
+		var form 	= document.createElement('form');
+		form.setAttribute('action',this.action_uri);
+		form.setAttribute('method',this.method);
+		form.setAttribute('class',this.class_name);
+
+		// reading fields data & generate component
+		for(var position = 0; position < this.fields.length; position++){
+			var field = this.generateField(this.fields[position]);
+			form.appendChild(field);
+		}
+
+		// adding default button
+		var text 	= "";
+		var button 	= document.createElement('button');
+		var text 	= document.createTextNode('Submit');
+		button.setAttribute('type','submit');
+		button.setAttribute('class','btn btn-default');
+		button.appendChild(text);
+		form.appendChild(button);
+
+		// finally add form element into container.
+		container.appendChild(form);
 	}
 
 
