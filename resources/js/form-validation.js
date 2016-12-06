008782744afafb335e50389cc1820d93d5bbb963
Zuriel.Diaz <zuriel.diaz@gea.mx>
2016-12-05 18:13:51 -0600

    refactor: get data from form element.

--- resources/js/form-validation.js
+++ resources/js/form-validation.js
@@ -18,25 +18,22 @@ function sendData(data){
 }
 
 function main(){
-	var form_prefix = "form#", form_id = "fm-exp";
-	$(form_prefix+form_id).submit(function(evt){
+	$("form").submit(function(evt){
 		var data = {};
+		evt.preventDefault();
 
 		// get data table name & action type (this means 'insert' or 'update' action).
-		data.table_name = $(form_prefix+form_id).attr("data-tn");
-		data.action_type = $(form_prefix+form_id).attr("data-action-type");
+        data.table_name   = $(this).attr("data-tn");
+        data.action_type  = $(this).attr("data-action-type");
 
-		// now we need to get the field names from 'data-field-name' attribute
-		$((form_prefix+form_id)+" input[type=text]").each(function(){
-			var key 	= $(this).attr("data-field-name");
-			var value 	= $(this).val();
-			if(key && value){ data[key] = value; }
-		});
-
+        $("form input[type=text]").each(function(key,field){
+            var key = $(field).attr("data-field-name");
+            var value = $(field).val();
+            if(key && value){ data[key] = value; }
+        });
+        console.log(data);
 		// send data through AJAX
 		sendData(data);
-
-		evt.preventDefault();
 	});
 }
 
